d1c5b838a80b29a792d6974bc73eebed52bc2597
Malo Lebrin <malolebrin@gmail.com>
2020-12-28 15:34:34 +0100

    begining

--- App.tsx
+++ App.tsx
@@ -1,21 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { StyleSheet, Text, View } from 'react-native';
 
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
 export default function App() {
+  const [isLoading, setIsLoading] = useState(true)
+  const [userToken, setUserToken] = useState(String)
+  const [user, setUser] = useState(Object)
+
+  const setToken = async (token: string) => {
+    if (token) {
+      await AsyncStorage.setItem('userToken', token)
+    } else {
+      await AsyncStorage.removeItem('userToken');
+    }
+    setUserToken(token);
+  }
+
+  const setUserData = async (user: Object) => {
+    if (user) {
+      const USER = JSON.stringify(user);
+      await AsyncStorage.setItem('user', USER);
+    } else {
+      await AsyncStorage.removeItem('user')
+    }
+    setUser(user)
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+        {/* <Stack.Navigator initialRouteName="SignIn">
+          <Stack.Screen
+            name="SignIn"
+            options={{ header: () => null, animationEnabled: false }}
+          >
+            {() => <SignInScreen setId={setId} setToken={setToken} />}
+          </Stack.Screen>
+        </Stack.Navigator> */}
+      </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
