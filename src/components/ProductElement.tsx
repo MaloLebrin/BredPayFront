aeca4a048617a381d6d4c5e17d832f1c79e7289d
Malo Lebrin <malolebrin@gmail.com>
2020-12-30 08:04:21 +0100

    init product card element to use it in homescreen

---
+++ src/components/ProductElement.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { View, Text, ActivityIndicator, Image, StyleSheet } from "react-native";
+import Loader from './Loader'
+import Colors from '../assets/colors'
+
+type ProductElementType = {
+    elementData: elementData,
+}
+interface elementData {
+    photo: any,
+    productName: string,
+}
+
+const ProductElement = ({ elementData } : ProductElementType) => {
+    return elementData ? (
+        <View>
+            <Text>{elementData.productName}</Text>
+            <Image 
+                style={styles.img}
+                source={{ uri: elementData.photo[0].result.secure_url }}            
+            />
+        </View>
+    ) : 
+    (<Loader />)
+}
+const styles = StyleSheet.create({
+    img : {
+        height: 50,
+        width: 50,
+        borderRadius: 30,
+        borderWidth: 1,
+        borderColor: Colors.greyDark
+    }
+})
+export default ProductElement
